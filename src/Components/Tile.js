bdb692c23d3605cd967524be9f9df74be15b02b3
= <larrybuntus@gmail.com>
2020-08-16 06:53:50 +0000

    hot fixes: updated tiles implementation

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,7 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 
-const StyledTypes = styled.span`
+const Tile = styled.span`
   background-color: ${({ active }) => (active ? "#05051B" : "#f0f6f9")};
   color: ${({ active }) => (active ? "#ffffff" : "#000000")};
   transition: ease all 0.25s;
@@ -12,9 +11,5 @@ const StyledTypes = styled.span`
   cursor: pointer;
   padding: 22px;
 `;
-
-const Tile = ({ children, active }) => {
-  return <StyledTypes {...{ active }}>{children}</StyledTypes>;
-};
 
 export default Tile;
