755ec4c963083931bfdad04200953ab9eb6837fd
= <larrybuntus@gmail.com>
2020-09-19 12:05:06 +0000

    hot fixes: removed animation from navbar

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,8 +1,6 @@
 import React, { Fragment, useGlobal } from "reactn";
 import { Container, Nav } from "react-bootstrap";
 
-import Fade from "react-reveal";
-
 import {
   NavLink,
   NavbarBrand,
@@ -28,99 +26,97 @@ const Navbar = ({
 
   return (
     <StyledNavbar collapseOnSelect expand="md" bg="light" variant="light">
-      <Fade bottom>
-        <Container>
-          <NavbarBrand className="cursor-pointer" onClick={() => router("/")}>
-            <Icon.Logo />
-          </NavbarBrand>
-          <NavbarToggle aria-controls="navigation" />
-          <NavbarCollapse id="navigation">
-            <Nav className="ml-auto">
-              {items?.map((item, key) => (
-                <Fragment key={key}>
-                  {item.menu ? (
-                    <NavDropdown
-                      title={
-                        <Fragment>
-                          <span>{item.name}</span>
-                          <i className="material-icons-round font-size-18">
-                            expand_more
-                          </i>
-                        </Fragment>
-                      }
-                      id="collasible-nav-dropdown"
-                    >
-                      {item.menu?.map((menu, key) => (
-                        <NavDropdownItem
-                          {...(!menu.target && {
-                            onClick: (e) =>
-                              e.preventDefault() | router(menu.link),
-                          })}
-                          href={menu.link}
-                          key={key}
-                        >
-                          {menu.name}
-                        </NavDropdownItem>
-                      ))}
-                    </NavDropdown>
-                  ) : (
-                    <NavLink
-                      {...(!item.target && {
-                        onClick: (e) => e.preventDefault() | router(item.link),
-                      })}
-                      href={item.link}
-                    >
-                      {item.name}
-                    </NavLink>
-                  )}
-                </Fragment>
-              ))}
+      <Container>
+        <NavbarBrand className="cursor-pointer" onClick={() => router("/")}>
+          <Icon.Logo />
+        </NavbarBrand>
+        <NavbarToggle aria-controls="navigation" />
+        <NavbarCollapse id="navigation">
+          <Nav className="ml-auto">
+            {items?.map((item, key) => (
+              <Fragment key={key}>
+                {item.menu ? (
+                  <NavDropdown
+                    title={
+                      <Fragment>
+                        <span>{item.name}</span>
+                        <i className="material-icons-round font-size-18">
+                          expand_more
+                        </i>
+                      </Fragment>
+                    }
+                    id="collasible-nav-dropdown"
+                  >
+                    {item.menu?.map((menu, key) => (
+                      <NavDropdownItem
+                        {...(!menu.target && {
+                          onClick: (e) =>
+                            e.preventDefault() | router(menu.link),
+                        })}
+                        href={menu.link}
+                        key={key}
+                      >
+                        {menu.name}
+                      </NavDropdownItem>
+                    ))}
+                  </NavDropdown>
+                ) : (
+                  <NavLink
+                    {...(!item.target && {
+                      onClick: (e) => e.preventDefault() | router(item.link),
+                    })}
+                    href={item.link}
+                  >
+                    {item.name}
+                  </NavLink>
+                )}
+              </Fragment>
+            ))}
 
-              {isAuthenticated ? (
-                <NavDropdown
-                  title={
-                    <Fragment>
-                      <div className="profile__avatar" />
-                      <i className="material-icons-round font-size-18">
-                        expand_more
-                      </i>
-                    </Fragment>
-                  }
-                  id="collasible-nav-dropdown"
+            {isAuthenticated ? (
+              <NavDropdown
+                title={
+                  <Fragment>
+                    <div className="profile__avatar" />
+                    <i className="material-icons-round font-size-18">
+                      expand_more
+                    </i>
+                  </Fragment>
+                }
+                id="collasible-nav-dropdown"
+              >
+                <NavDropdownItem
+                  href={`${process.env.REACT_APP_AUTH_URL}/profile`}
                 >
-                  <NavDropdownItem
-                    href={`${process.env.REACT_APP_AUTH_URL}/profile`}
-                  >
-                    Account
-                  </NavDropdownItem>
-                  <NavDropdownItem className="red-text" onClick={logout}>
-                    Logout
-                  </NavDropdownItem>
-                </NavDropdown>
-              ) : (
-                <Fragment>
-                  <NavLink
-                    href={`${process.env.REACT_APP_AUTH_URL}/signup${
-                      app ? `?app=${app}` : ""
-                    }`}
-                    className="font-weight-600"
-                  >
-                    Signup
-                  </NavLink>
-                  <NavLink
-                    href={`${process.env.REACT_APP_AUTH_URL}/login${
-                      app ? `?app=${app}` : ""
-                    }`}
-                    className="btn btn--default"
-                  >
-                    Login
-                  </NavLink>
-                </Fragment>
-              )}
-            </Nav>
-          </NavbarCollapse>
-        </Container>
-      </Fade>
+                  Account
+                </NavDropdownItem>
+                <NavDropdownItem className="red-text" onClick={logout}>
+                  Logout
+                </NavDropdownItem>
+              </NavDropdown>
+            ) : (
+              <Fragment>
+                <NavLink
+                  href={`${process.env.REACT_APP_AUTH_URL}/signup${
+                    app ? `?app=${app}` : ""
+                  }`}
+                  className="font-weight-600"
+                >
+                  Signup
+                </NavLink>
+                <NavLink
+                  href={`${process.env.REACT_APP_AUTH_URL}/login${
+                    app ? `?app=${app}` : ""
+                  }`}
+                  className="btn btn--default"
+                >
+                  Login
+                </NavLink>
+              </Fragment>
+            )}
+          </Nav>
+        </NavbarCollapse>
+      </Container>
     </StyledNavbar>
   );
 };
