f82498dcd243fcdbefa2e31e3d0b951df97cd028
= <larrybuntus@gmail.com>
2020-09-30 20:08:32 +0000

    updates: added icons

---
+++ src/Icons/AlignLeft.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const AlignLeft = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11.3333 6.66602H2"
+      stroke="#788A9C"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M14 4H2"
+      stroke="#788A9C"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M14 9.33398H2"
+      stroke="#788A9C"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M11.3333 12H2"
+      stroke="#788A9C"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default AlignLeft;
--- src/Icons/index.js
+++ src/Icons/index.js
@@ -1,13 +1,16 @@
+export { default as Edit } from "./Edit";
 export { default as Logo } from "./Logo";
 export { default as Close } from "./Close";
 export { default as Image } from "./Image";
 export { default as Smiley } from "./Smiley";
 export { default as Upload } from "./Upload";
+export { default as Person } from "./Person";
 export { default as Comment } from "./Comment";
 export { default as Twitter } from "./Twitter";
 export { default as Youtube } from "./Youtube";
 export { default as Facebook } from "./Facebook";
 export { default as LinkedIn } from "./LinkedIn";
+export { default as AlignLeft } from "./AlignLeft";
 export { default as Instagram } from "./Instagram";
 export { default as Expertise } from "./Expertise";
 export { default as Portfolio } from "./Portfolio";
