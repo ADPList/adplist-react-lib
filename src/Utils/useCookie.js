58248be315420ee59b37a15d033135a8fb8cd4f8
= <larrybuntus@gmail.com>
2020-10-23 02:06:14 +0000

    hot fixes: useCookie updated to support nextjs

--- src/Utils/useCookie.js
+++ src/Utils/useCookie.js
@@ -1,20 +1,28 @@
+const isBrowser = () => typeof window !== "undefined";
+
 const useCookie = () => {
   const setCookie = (key, value) => {
-    window.document.cookie = `${key}=${value}; domain=${process.env.REACT_APP_PARENT_URL}; path=/`;
+    if (isBrowser) {
+      return (window.document.cookie = `${key}=${value}; domain=${process.env.REACT_APP_PARENT_URL}; path=/`);
+    }
   };
 
   const getCookie = (key) => {
-    return (
-      window.document.cookie
-        ?.split("; ")
-        ?.find((row) => row.startsWith(key))
-        ?.split("=")[1] || null
-    );
+    if (isBrowser) {
+      return (
+        window.document.cookie
+          ?.split("; ")
+          ?.find((row) => row.startsWith(key))
+          ?.split("=")[1] || null
+      );
+    }
   };
 
   const deleteCookie = (key) => {
-    if (getCookie(key)) {
-      window.document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=${process.env.REACT_APP_PARENT_URL}; path=/`;
+    if (isBrowser) {
+      if (getCookie(key)) {
+        return (window.document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; domain=${process.env.REACT_APP_PARENT_URL}; path=/`);
+      }
     }
   };
 
