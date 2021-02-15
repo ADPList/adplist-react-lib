1c5282f53ac7a3162033606bd9e606534b02cc99
lawrenceadu <larrybuntus@gmail.com>
2021-02-15 18:24:53 +0000

    hot fixes: added seen to notification service

--- src/Services/notificationService.js
+++ src/Services/notificationService.js
@@ -1,4 +1,6 @@
 import Http from "../Utils/Http";
 
 export const updateNotificationSeenService = (id) =>
-  Http.put(`${process.env.REACT_APP_MESSAGING_BASEURL}/notification/${id}/`);
+  Http.put(`${process.env.REACT_APP_MESSAGING_BASEURL}/notification/${id}/`, {
+    seen: true,
+  });
