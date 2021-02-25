e2a9e99ea8ca3c2c3b29cdb85a5feed6b22ddcde
abigailedwin <57915480+abigailedwin@users.noreply.github.com>
2021-02-25 09:37:53 +0000

    Formatted date picker

--- src/Components/DatePicker.js
+++ src/Components/DatePicker.js
@@ -1,25 +1,16 @@
-import React, {Fragment} from 'react'
-import {Form} from "react-bootstrap"
+import React, { Fragment } from "react";
+import { Form } from "react-bootstrap";
 import Datetime from "react-datetime";
 
 import "react-datetime/css/react-datetime.css";
 
-const DateTimePicker = ({label, ...props}) => {
-    return (
-        <Fragment>
-             {label && <Form.Label>{label}</Form.Label>}
-            <Datetime 
-            dateFormat="DD-MM-YY"
-            timeFormat={false}
-            onChange={(value) =>
-            setFieldValue(value)
-            }
-            {...(maxDate)}
-            {...(minDate)}
-            {...props}
-             />
-        </Fragment>
-    )
-}
+const DateTimePicker = ({ label, ...props }) => {
+  return (
+    <Fragment>
+      {label && <Form.Label>{label}</Form.Label>}
+      <Datetime dateFormat="DD-MM-YY" timeFormat={false} {...props} />
+    </Fragment>
+  );
+};
 
 export default DateTimePicker;
--- src/Components/TimePicker.js
+++ src/Components/TimePicker.js
@@ -1,15 +1,9 @@
-import React from 'react'
+import React from "react";
 import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 
-const TimePicker = ({ ...props}) => {
-    return (
-        <Datetime 
-        timeFormat
-        dateFormat={false}
-        {...props} 
-         />
-    )
-}
+const TimePicker = ({ ...props }) => {
+  return <Datetime timeFormat dateFormat={false} {...props} />;
+};
 
 export default TimePicker;
