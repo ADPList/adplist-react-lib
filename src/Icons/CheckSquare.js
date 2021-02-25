8c2c86f03638d0a125318e32b1a0f9516c629981
adjoaEdwin <aedwin905@gmail.com>
2021-02-25 16:52:55 +0000

    Replaced the checkquare to the correct svg

--- src/Icons/CheckSquare.js
+++ src/Icons/CheckSquare.js
@@ -15,15 +15,21 @@ const CheckSquare = ({ size, color, ...props }) => (
   <svg
     width={size}
     height={size}
-    viewBox="0 0 36 36"
+    viewBox="0 0 44 44"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}
   >
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M25.9464 1.04205H10.0533C4.51478 1.04205 1.04245 4.96355 1.04245 10.5131V25.4877C1.04245 31.0372 4.49828 34.9587 10.0533 34.9587H25.9446C31.5014 34.9587 34.9591 31.0372 34.9591 25.4877V10.5131C34.9591 4.96355 31.5014 1.04205 25.9464 1.04205Z"
+      d="M29.9464 5.04205H14.0533C8.51478 5.04205 5.04245 8.96355 5.04245 14.5131V29.4877C5.04245 35.0372 8.49828 38.9587 14.0533 38.9587H29.9446C35.5014 38.9587 38.9591 35.0372 38.9591 29.4877V14.5131C38.9591 8.96355 35.5014 5.04205 29.9464 5.04205Z"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15.4732 22.0004L19.8255 26.3509L28.5265 17.6499"
       stroke={color}
       strokeWidth="1.5"
       strokeLinecap="round"
